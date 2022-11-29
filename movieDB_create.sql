8a28246d156fa117f1cff59fae4d70e953cf71e0
ojenny001 <76223794+ojenny001@users.noreply.github.com>
2022-11-28 22:32:11 -0500

    Add files via upload

--- movieDB_create.sql
+++ movieDB_create.sql
@@ -7,14 +7,14 @@ create table GenreType (
     );
 
 create table Movie (
-  movie_id int not null auto_increment,
+  movie_id int null auto_increment,
   title varchar(100) not null,
   release_year int not null,
   primary key (movie_id)
 );
 
 create table Director (
-    director_id int not null auto_increment,
+    director_id int null auto_increment,
     director_name varchar(100) not null,
     director_birth_date date not null,
     director_gender varchar(10) not null,
@@ -22,7 +22,7 @@ create table Director (
     );
 
 create table Actor (
-    actor_id int not null auto_increment,
+    actor_id int null auto_increment,
     actor_name varchar(100) not null,
     birth_date date not null,
     gender varchar(10) not null,
@@ -30,30 +30,30 @@ create table Actor (
     );
 
 create table ActorNationality (
-    actor_id int not null,
+    actor_id int null,
     nationality varchar(50) not null,
     primary key (actor_id, nationality),
     foreign key (actor_id) references Actor(actor_id)
     );
 
 create table MovieDirector (
-    movie_id int not null,
-    director_id int not null,
+    movie_id int null,
+    director_id int null,
     primary key (movie_id, director_id),
     foreign key (movie_id) references Movie(movie_id),
     foreign key (director_id) references Director(director_id)
     );
 
 create table MovieCast (
-    movie_id int not null,
-    cast_id int not null,
+    movie_id int null,
+    cast_id int null,
     primary key (movie_id, cast_id),
     foreign key (movie_id) references Movie(movie_id),
     foreign key (cast_id) references Actor(actor_id)
     );
 
 create table MovieGenre (
-    movie_id int not null,
+    movie_id int null,
     genre varchar(50) not null,
     primary key (movie_id, genre),
     foreign key (movie_id) references Movie(movie_id),
@@ -61,14 +61,14 @@ create table MovieGenre (
     );
 
 create table ProductionCountry (
-    movie_id int not null,
+    movie_id int null,
     country varchar(50) not null,
     primary key (movie_id, country),
     foreign key (movie_id) references Movie(movie_id)
     );
 
 create table OriginalLanguage (
-    movie_id int not null,
+    movie_id int null,
     language varchar(50) not null,
     primary key (movie_id, language),
     foreign key (movie_id) references Movie(movie_id)
